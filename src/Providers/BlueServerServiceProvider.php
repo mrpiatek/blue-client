0a67777e94ff00acefb5e7e4769cedddf1a0697c
mp <mariusz.piatek92@gmail.com>
2018-04-19 17:26:29 +0200

    Initial commit

---
+++ src/Providers/BlueServerServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MrPiatek\BlueServer\Providers;
+
+
+class BlueClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //$this->loadRoutesFrom(__DIR__.'/../routes.php');
+    }
+}
